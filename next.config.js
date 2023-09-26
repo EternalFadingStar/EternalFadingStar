f299b4c25194e71e9f86e84471b52c4b6ed8c59b
EternalFadingStar <Fadingstarrr@gmail.com>
2023-09-25 23:06:31 -0400

    Deployments

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
+module.exports = nextConfig
+
 module.exports = {
-    // Use the prefix for assets and links to pages
-    basePath: '/EternalFadingStar',
-    assetPrefix: '/EternalFadingStar/',
+    output: 'export'
   };
-    
+  
